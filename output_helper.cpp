1c4d2fa22ed0cf9bb1d8650aee72a737d2bc5631
BullTheRockLord <timrmiles@gmail.com>
2020-08-18 22:12:56 -0400

    Added a place to put future helper methods for output

---
+++ output_helper.cpp
@@ -0,0 +1,8 @@
+#include <sstream>
+#include <iomanip>
+
+char hex_characters[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+std::string byte_to_hex(char byte){
+	return std::string("CONVERT TO HEX");
+}
